466117f6eccf506a4a8dc64a733da88c1f30d9b0
AutoCloneBot <acautoclone@bot.com>
2025-10-05 14:51:17 +0000

    Added my codes from AtCoder

---
+++ abc426/abc426_a.py
@@ -0,0 +1,11 @@
+"""
+https://atcoder.jp/contests/abc426/tasks/abc426_a
+"""
+
+os = {"Ocelot": 1, "Serval": 2, "Lynx": 3}
+X, Y  = input().split()
+
+if os[X] >= os[Y]:
+    print("Yes")
+else:
+    print("No")
---
+++ abc426/abc426_b.py
@@ -0,0 +1,19 @@
+"""
+https://atcoder.jp/contests/abc426/tasks/abc426_b
+"""
+
+S = list(str(input()))
+
+s_0 = S[0]
+s_1 = ""
+flg = False
+for s in S[1:len(S)]:
+    if s_0 != s:
+        s_1 = s
+    if s_0 == s:
+        flg = True
+
+if flg:
+    print(s_1)
+else:
+    print(s_0)
